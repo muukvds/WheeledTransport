7c37715d84499243c63d6ed607bb8fbf5c1f88ca
muukvds <muukvds@users.noreply.github.com>
2017-09-26 16:14:26 +0200

    create Main class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        WheeledTransport transport = new WheeledTransport("car","Red");
+        WheeledTransport transport1 = new WheeledTransport("scouter","Blue");
+        WheeledTransport transport2 = new WheeledTransport("truck","Gray");
+
+    }
+}
